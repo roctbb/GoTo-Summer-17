800d56d2fc954b976d58b420e41851d04e7dffb1
roctbb <roctbb@gmail.com>
2017-06-20 12:24:55 +0300

    add day 7

--- day 7/guess_number.py
+++ day 7/guess_number.py
@@ -5,29 +5,44 @@ import telebot
 token = "322921279:AAEOFAsRz9jg3mImCzjrZzB1vS7HWpzA-p4"
 bot = telebot.TeleBot(token)
 
-numbers = {}
+data = {}
+
+
+def init_user(id):
+    if id not in data.keys():
+        data[id] = {"number": 0, "wins": 0, "tries": 0}
+
 
 def make_number(id):
-    numbers[id] = random.randint(1, 20)
+    data[id]["number"] = random.randint(1, 20)
+    data[id]["tries"] = 0
     bot.send_message(id, "Я загадал число, попробуй отгадать.")
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
+    init_user(message.chat.id)
     make_number(message.chat.id)
+
 
 @bot.message_handler(content_types=["text"])
 def text_message(message):
     id = message.chat.id
-    if id in numbers.keys():
-        if int(message.text) == numbers[id]:
-            bot.send_message(id, "Ты угадал!")
-            make_number(id)
-        else:
-            bot.send_message(id, "Неверно азаз!")
-    else:
+    init_user(id)
+    try:
+        number = int(message.text)
+    except:
+        bot.send_message(id, "Это походу не число :(")
+        return
+
+    if number == data[id]["number"]:
+        data[id]["wins"] += 1
+        bot.send_message(id, "Ты отгадал за {tries} попыток! Отгадано {wins} чисел."
+                         .format(tries=data[id]['tries'], wins=data[id]['wins']))
         make_number(id)
-
-
+    else:
+        data[id]["tries"] += 1
+        bot.send_message(id, "Неверно! использовано {tries} попыток."
+                         .format(tries=data[id]["tries"]))
 
 bot.polling(none_stop=True)
